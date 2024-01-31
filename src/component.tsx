5ac4f38ffaa90c251d3760dee61650ed9c1c46cf
Khar1tonov-Andrey <ander123345454@mail.ru>
2024-01-31 18:39:34 +0300

    global_setts

--- src/component.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { IConferenceDateWidget } from "@bauman-conference-library/interface/dist/confrence-date-widget";
-
-export const ConferenceDateWidget = (props: IConferenceDateWidget) =>{
-  return (
-    <div>{props.date.toString()}</div>
-  )
-}
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
-import { IConferenceDateWidget } from "@bauman-conference-library/interface/dist/confrence-date-widget";
+import React, { FC } from 'react';
+import { WidgetsFactory, ConferenceDateProps, NavbarProps} from '@bauman-conference-library/interface';
 
-export const ConferenceDateWidget = (props: IConferenceDateWidget) =>{
+export const ConferenceDateWidget = (props: ConferenceDateProps): JSX.Element => {
   return (
-    <div>{props.date.toString()}</div>
+    <div>
+      <div>{props.format}</div>
+      <div>{props.date.toString()}</div>
+    </div>
   )
+}
+
+export class LibFactory implements WidgetsFactory {
+  
+  getConferenceDateWidget(): FC<ConferenceDateProps> {
+    return ConferenceDateWidget;
+  }
+
+  getNavbar(): FC<NavbarProps> {
+    throw new Error("not implemented")
+  }
+
 }
