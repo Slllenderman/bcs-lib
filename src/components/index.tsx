3a9dc16385b9c98ab52306e8ac21d35c27f09b0b
Khar1tonov-Andrey <ander123345454@mail.ru>
2024-05-04 22:59:00 +0300

    next major version

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,11 +2,18 @@ import { Navbar } from "./navbar";
 import { ConferenceDate } from "./datetime"
 import { Footer } from "./footer"
 import { Heading } from "./heading"
+import { Logo } from "./logo";
+import { ConferenceDescription } from "./confdescription"
+import { ConferenceLocation } from "./conflocation";
+import { UserCard } from "./userCard"
 
-export { Navbar, ConferenceDate, Footer, Heading }
+export { Navbar, ConferenceDate, Footer, Heading, Logo, ConferenceDescription, ConferenceLocation, UserCard }
 
-import { Link } from "@mui/material"
-
+import { Link, Grid, Typography, Container } from "@mui/material"
+ 
 export const mui = {
-    Link
+    Link,
+    Grid,
+    Typography,
+    Container
 }
